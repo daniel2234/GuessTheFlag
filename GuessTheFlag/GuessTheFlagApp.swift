d691c9c69a0cb3d9941b89746e111320a6fdc3b5
daniel2234 <kwiatkowski.daniel@gmail.com>
2022-01-24 13:15:17 -0500

    Initial Commit

---
+++ GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by daniel on 2022-01-24.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
